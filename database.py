86c9bc4f5ec02daa69066bf3eb13092d6af81662
PhucRey235 <minhphucphan235@gmail.com>
2025-05-13 17:27:37 +0700

    init

--- database.py
+++ database.py
@@ -1,6 +1,16 @@
+import os
 import sqlalchemy as sa
 import streamlit as st
 
 @st.cache_resource
 def get_engine():
-    return sa.create_engine('sqlite:///warehouse.db')
+    db_path = os.path.join(os.getcwd(), "warehouse.db")
+
+    # Nếu file chưa tồn tại, tạo file rỗng
+    if not os.path.exists(db_path):
+        open(db_path, 'a').close()
+
+    # Cấp quyền ghi/đọc/thực thi cho tất cả user (rwxrwxrwx)
+    os.chmod(db_path, 0o777)
+
+    return sa.create_engine(f"sqlite:///{db_path}")
